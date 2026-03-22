6da582f8937784425de59807894c619b05a898d5
Raminadif1 <Rami.nadif@gmail.com>
2026-03-22 17:25:16 +0200

    2tehtävä tehty

--- react/tehtävä 2/src/App.jsx
+++ react/tehtävä 2/src/App.jsx
@@ -1,25 +1,11 @@
 import Tervehdys from "./tervehdys";
+import Opiskelijatiedot from "./opiskelijatiedot";
 
 function App() {
   return (
     <div>
       <Tervehdys nimi="Ville" />
       <Opiskelijatiedot />
-    </div>
-  );
-}
-function Opiskelijatiedot() {
-  const opiskelija = {
-    nimi: "Matti Meikäläinen",
-    ika: 16,
-    kurssi: "Reactin perusteet",
-  };
-  return (
-    <div>
-      <h2>Opiskelijan tiedot</h2>
-      <p>Nimi: {opiskelija.nimi}</p>
-      <p>Ikä: {opiskelija.ika}</p>
-      <p>Kurssi: {opiskelija.kurssi}</p>
     </div>
   );
 }
---
+++ react/tehtävä 2/src/opiskelijatiedot.jsx
@@ -0,0 +1,16 @@
+function Opiskelijatiedot() {
+  const opiskelija = {
+    nimi: "Matti Meikäläinen",
+    ika: 16,
+    kurssi: "Reactin perusteet",
+  };
+  return (
+    <div>
+      <h2>Opiskelijan tiedot</h2>
+      <p>Nimi: {opiskelija.nimi}</p>
+      <p>Ikä: {opiskelija.ika}</p>
+      <p>Kurssi: {opiskelija.kurssi}</p>
+    </div>
+  );
+}
+export default Opiskelijatiedot;
