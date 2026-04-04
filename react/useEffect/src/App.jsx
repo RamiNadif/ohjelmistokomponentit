901a61e9ef2177c7080efefc5d4cb1220372a1f7
Raminadif1 <Rami.nadif@gmail.com>
2026-04-04 19:23:24 +0300

    kayttajav2

--- react/useEffect/src/App.jsx
+++ react/useEffect/src/App.jsx
@@ -1,12 +1,14 @@
 import Title from "./title";
 import Postaukset from "./postaukset";
 import Kayttajatiedot from "./kayttajatiedot";
+import Kayttajatiedotv2 from "./kayttajatiedotV2";
 function App() {
   return (
     <div>
       <Title />
       <Postaukset />
       <Kayttajatiedot />
+      <Kayttajatiedotv2 />
     </div>
   );
 }
--- react/useEffect/src/kayttajatiedotV2.jsx
+++ react/useEffect/src/kayttajatiedotV2.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 
-function Kayttajatiedot() {
+function Kayttajatiedotv2() {
   const [id, setId] = useState("");
   const [tiedot, setTiedot] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+  const [posts, setPosts] = useState(null);
 
   const haeKayttaja = async () => {
     const userId = Number(id);
@@ -20,18 +21,25 @@ function Kayttajatiedot() {
 
     try {
       const response = await fetch(
-        `https://jsonplaceholder.typicode.com/usersss/${userId}`,
+        `https://jsonplaceholder.typicode.com/users/${userId}`,
+      );
+      const responsepost = await fetch(
+        `https://jsonplaceholder.typicode.com/posts?userId=${userId}`,
       );
 
-      if (!response.ok) {
+      if (!response.ok || !responsepost.ok) {
         throw new Error("Käyttäjän hakeminen epäonnistui");
       }
 
       const data = await response.json();
+      const datapost = await responsepost.json();
+      setPosts(datapost);
+
       setTiedot(data);
     } catch (err) {
       setError(err.message);
       setTiedot(null);
+      setPosts(null);
     } finally {
       setLoading(false);
     }
@@ -39,7 +47,7 @@ function Kayttajatiedot() {
 
   return (
     <div>
-      <h1>Käyttäjätiedot</h1>
+      <h1>Käyttäjätiedot ja Titlet</h1>
 
       <input
         type="number"
@@ -53,14 +61,20 @@ function Kayttajatiedot() {
       {loading && <p>Ladataan...</p>}
       {error && <p style={{ color: "red" }}>{error}</p>}
 
-      {tiedot && !loading && !error && (
+      {tiedot && posts && !loading && !error && (
         <p>
           Nimi: {tiedot.name} <br />
           Kaupunki: {tiedot.address?.city}
+          <h3>Titles</h3>
+          <ul>
+            {posts.slice(0, 10).map((post) => (
+              <li key={post.id}>{post.title}</li>
+            ))}
+          </ul>
         </p>
       )}
     </div>
   );
 }
 
-export default Kayttajatiedot;
+export default Kayttajatiedotv2;
